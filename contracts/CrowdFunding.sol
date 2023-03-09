2dd63ae806cad4aa2d42be130e7e22dff1d70e24
007blockchaindeveloper <Dovasy0325@gmail.com>
2023-03-09 23:10:35 +0700

    Simple crowdfunding project

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+error InvalidTokenAddress();
+error InvalidFeeScaleFactor();
+error InvalidFeeRate();
+error CallerNotProjectOwner();
+error InvalidGoal();
+error InvalidDeadline(uint256 minPossible);
+error ProjectAlreadyEnded();
+error ProjectNotEndedYet();
+error DeadlineAlreadyPassed();
+error DeadlineNotPassedYet();
+error InvalidAmount();
+error CannotWithdrawFromSuccessfulProject();
+error InvalidProjectId();
+
+contract CrowdFunding is Ownable {
+    using SafeERC20 for IERC20;
+
+    struct Project {
+        uint256 id;
+        uint256 goal;
+        uint256 deadline;
+        uint256 amountRaised;
+        address owner;
+        bool ended;
+        bool succeeded;
+    }
+
+    struct Contribution {
+        address contributor;
+        uint256 amount;
+    }
+
+    uint256 public numProjects;
+    mapping (uint256 => Project) public projects;
+    mapping (uint256 => mapping (address => Contribution)) public contributions;
+
+    IERC20 public immutable TOKEN;
+    uint256 public immutable FEE_RATE;
+    uint256 public immutable FEE_SCALE_FACTOR;
+
+    constructor(address tokenAddress, uint256 feeRate, uint256 feeScaleFactor) {
+        if (tokenAddress == address(0)) revert InvalidTokenAddress();
+        if (feeScaleFactor == 0) revert InvalidFeeScaleFactor();
+        if (feeRate >= feeScaleFactor) revert InvalidFeeRate();
+
+        TOKEN = IERC20(tokenAddress);
+        FEE_RATE = feeRate;
+        FEE_SCALE_FACTOR = feeScaleFactor;
+    }
+
+    event ProjectCreated(uint256 projectId, address owner, uint256 goal, uint256 deadline);
+    event ProjectFunded(uint256 projectId, address contributor, uint256 amount);
+    event ProjectEnded(uint256 projectId, bool succeeded);
+
+    modifier onlyProjectOwner(uint256 projectId) {
+        if (msg.sender != projects[projectId].owner) revert CallerNotProjectOwner();
+        _;
+    }
+
+    function createProject(uint256 goal, uint256 deadline) external {
+        if (goal == 0) revert InvalidGoal();
+        if (deadline <= block.timestamp) revert InvalidDeadline(block.timestamp);
+
+        numProjects++;
+        projects[numProjects] = Project(numProjects, goal, deadline, 0, msg.sender, false, false);
+
+        emit ProjectCreated(numProjects, msg.sender, goal, deadline);
+    }
+
+    function fundProject(uint256 projectId, uint256 amount) external {
+        if (projectId == 0 || projectId > numProjects) revert InvalidProjectId();
+
+        Project storage project = projects[projectId];
+        if (project.ended) revert ProjectAlreadyEnded();
+        if (project.deadline <= block.timestamp) revert DeadlineAlreadyPassed();
+        if (amount == 0) revert InvalidAmount();
+
+        Contribution storage contribution = contributions[projectId][msg.sender];
+        contribution.contributor = msg.sender;
+        contribution.amount += amount;
+        project.amountRaised += amount;
+
+        emit ProjectFunded(projectId, msg.sender, amount);
+
+        TOKEN.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function endProject(uint256 projectId) external onlyProjectOwner(projectId) {
+        if (projectId == 0 || projectId > numProjects) revert InvalidProjectId();
+
+        Project storage project = projects[projectId];
+        if (project.ended) revert ProjectAlreadyEnded();
+        if (block.timestamp < project.deadline) revert DeadlineNotPassedYet();
+
+        project.ended = true;
+        bool succeeded = project.amountRaised >= project.goal;
+        project.succeeded = succeeded;
+
+        if (succeeded) {
+            uint256 fee = project.amountRaised * FEE_RATE / FEE_SCALE_FACTOR;
+            uint256 amountToOwner = project.amountRaised - fee;
+
+            emit ProjectEnded(projectId, true);
+
+            TOKEN.safeTransfer(project.owner, amountToOwner);
+            TOKEN.safeTransfer(owner(), fee);
+        } else {
+            emit ProjectEnded(projectId, false);
+        }
+    }
+
+    function withdrawFunds(uint256 projectId) external {
+        if (projectId == 0 || projectId > numProjects) revert InvalidProjectId();
+
+        Project storage project = projects[projectId];
+        if (!project.ended) revert ProjectNotEndedYet();
+        if (project.succeeded) revert CannotWithdrawFromSuccessfulProject();
+
+        Contribution storage contribution = contributions[projectId][msg.sender];
+        uint256 amountToWithdraw = contribution.amount;
+        if (amountToWithdraw > 0) {
+            project.amountRaised -= amountToWithdraw;
+            contribution.amount = 0;
+
+            emit ProjectFunded(projectId, msg.sender, 0);
+            
+            TOKEN.safeTransfer(msg.sender, amountToWithdraw);
+        }
+    }
+}
---
+++ test/CrowdFunding.js
@@ -0,0 +1,165 @@
+const { time, loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+
+describe("CrowdFunding", function () {
+    const goal = 1500;
+    const feeRate = 1;
+    const feeScaleFactor = 100;
+
+    async function deployAndCreateProject() {
+        const [owner, alice, bob, john] = await ethers.getSigners();
+
+        const MyToken = await hre.ethers.getContractFactory("MyToken");
+        const myToken = await MyToken.deploy();
+        await myToken.deployed();
+
+        console.log(
+            `MyToken deployed to ${myToken.address}`
+        );
+
+        myToken.mint(bob.address, 1000);
+        myToken.mint(john.address, 1000);
+
+        const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
+        const crowdFunding = await CrowdFunding.deploy(myToken.address, feeRate, feeScaleFactor);
+        await crowdFunding.deployed();
+
+        expect(await crowdFunding.owner()).to.equal(owner.address);
+
+        const ONE_DAY_IN_SECS = 24 * 60 * 60;
+        const deadline = (await time.latest()) + ONE_DAY_IN_SECS;
+        await expect(crowdFunding.connect(alice).createProject(goal, deadline)).to.emit(crowdFunding, "ProjectCreated").withArgs(1, alice.address, goal, deadline);
+
+        return { crowdFunding, myToken, deadline, owner, alice, bob, john };
+    }
+
+    describe("Create Project", function () {
+        it("Should create a new project", async function () {
+            const { crowdFunding, deadline, alice } = await loadFixture(deployAndCreateProject);
+    
+            expect(await crowdFunding.numProjects()).to.equal(1);
+
+            const project = await crowdFunding.projects(1);
+            expect(project.id).to.equal(1);
+            expect(project.goal).to.equal(goal);
+            expect(project.deadline).to.equal(deadline);
+            expect(project.amountRaised).to.equal(0);
+            expect(project.owner).to.equal(alice.address);
+            expect(project.ended).to.equal(false);
+            expect(project.succeeded).to.equal(false);
+        });
+    
+        it("Fails with invalid parameters", async function () {
+            const { crowdFunding, deadline } = await loadFixture(deployAndCreateProject);
+    
+            await expect(crowdFunding.createProject(0, deadline)).to.be.revertedWithCustomError(crowdFunding, "InvalidGoal");
+            await expect(crowdFunding.createProject(goal, await time.latest())).to.be.revertedWithCustomError(crowdFunding, "InvalidDeadline");
+        });
+    });
+
+    describe("Fund Project", function () {
+        it("Should transfer the token and increase the contribution", async function () {
+            const { crowdFunding, myToken, bob } = await loadFixture(deployAndCreateProject);
+    
+            await myToken.connect(bob).approve(crowdFunding.address, 1000);
+            await expect(crowdFunding.connect(bob).fundProject(1, 900)).to.emit(crowdFunding, "ProjectFunded").withArgs(1, bob.address, 900);
+
+            const contribution = await crowdFunding.contributions(1, bob.address);
+            expect(contribution.contributor).to.equal(bob.address);
+            expect(contribution.amount).to.equal(900);
+            
+            expect(await myToken.balanceOf(bob.address)).to.equal(100);
+        });
+    
+        it("Fails with invalid project id or amount", async function () {
+            const { crowdFunding, myToken, bob } = await loadFixture(deployAndCreateProject);
+    
+            await myToken.connect(bob).approve(crowdFunding.address, 1000);
+            await expect(crowdFunding.connect(bob).fundProject(0, 900)).to.be.revertedWithCustomError(crowdFunding, "InvalidProjectId");
+            await expect(crowdFunding.connect(bob).fundProject(2, 900)).to.be.revertedWithCustomError(crowdFunding, "InvalidProjectId");
+            await expect(crowdFunding.connect(bob).fundProject(1, 0)).to.be.revertedWithCustomError(crowdFunding, "InvalidAmount");
+        });
+
+        it("Fails when the deadline is past", async function () {
+            const { crowdFunding, myToken, deadline, bob } = await loadFixture(deployAndCreateProject);
+
+            await time.increaseTo(deadline);
+    
+            await myToken.connect(bob).approve(crowdFunding.address, 1000);
+            await expect(crowdFunding.connect(bob).fundProject(1, 900)).to.be.revertedWithCustomError(crowdFunding, "DeadlineAlreadyPassed");
+        });
+    });
+
+    describe("End Project", function () {
+        it("Should end the project and transfer the token to the owner and project owner", async function () {
+            const { crowdFunding, deadline, myToken, owner, alice, bob, john } = await loadFixture(deployAndCreateProject);
+    
+            await myToken.connect(bob).approve(crowdFunding.address, 1000);
+            await myToken.connect(john).approve(crowdFunding.address, 1000);
+
+            await crowdFunding.connect(bob).fundProject(1, 1000);
+            await crowdFunding.connect(john).fundProject(1, 1000);
+
+            await time.increaseTo(deadline);
+
+            await expect(crowdFunding.connect(alice).endProject(1)).to.emit(crowdFunding, "ProjectEnded").withArgs(1, true);
+
+            const project = await crowdFunding.projects(1);
+            expect(project.ended).to.equal(true);
+            expect(project.succeeded).to.equal(true);
+
+            const fee = 2000 * feeRate / feeScaleFactor;
+            expect(await myToken.balanceOf(owner.address)).to.equal(fee);
+            expect(await myToken.balanceOf(alice.address)).to.equal(2000 - fee);
+        });
+    
+        it("Fails when the deadline is not passed", async function () {
+            const { crowdFunding, myToken, alice, bob } = await loadFixture(deployAndCreateProject);
+    
+            await myToken.connect(bob).approve(crowdFunding.address, 1000);
+            await crowdFunding.connect(bob).fundProject(1, 1000);
+            await expect(crowdFunding.connect(alice).endProject(1)).to.be.revertedWithCustomError(crowdFunding, "DeadlineNotPassedYet");
+        });
+    });
+
+    describe("Withdraw Funds", function () {
+        it("Should reset the contribution and withdraw the funds to the contributor", async function () {
+            const { crowdFunding, deadline, myToken, alice, bob } = await loadFixture(deployAndCreateProject);
+    
+            await myToken.connect(bob).approve(crowdFunding.address, 1000);
+            await crowdFunding.connect(bob).fundProject(1, 1000);
+            await time.increaseTo(deadline);
+            await crowdFunding.connect(alice).endProject(1);
+
+            expect(await myToken.balanceOf(bob.address)).to.equal(0);
+            await expect(crowdFunding.connect(bob).withdrawFunds(1)).to.emit(crowdFunding, "ProjectFunded").withArgs(1, bob.address, 0);
+            expect(await myToken.balanceOf(bob.address)).to.equal(1000);
+            
+            const contribution = await crowdFunding.contributions(1, bob.address);
+            expect(contribution.contributor).to.equal(bob.address);
+            expect(contribution.amount).to.equal(0);
+        });
+
+        it("Fails when the project is not ended yet", async function () {
+            const { crowdFunding, deadline, myToken, alice, bob } = await loadFixture(deployAndCreateProject);
+    
+            await myToken.connect(bob).approve(crowdFunding.address, 1000);
+            await crowdFunding.connect(bob).fundProject(1, 1000);
+
+            await expect(crowdFunding.connect(bob).withdrawFunds(1)).to.be.revertedWithCustomError(crowdFunding, "ProjectNotEndedYet");
+        });
+    
+        it("Fails when the project is successful", async function () {
+            const { crowdFunding, deadline, myToken, alice, bob, john } = await loadFixture(deployAndCreateProject);
+    
+            await myToken.connect(bob).approve(crowdFunding.address, 1000);
+            await myToken.connect(john).approve(crowdFunding.address, 1000);
+            await crowdFunding.connect(bob).fundProject(1, 1000);
+            await crowdFunding.connect(john).fundProject(1, 1000);
+            await time.increaseTo(deadline);
+            await crowdFunding.connect(alice).endProject(1);
+
+            await expect(crowdFunding.connect(bob).withdrawFunds(1)).to.be.revertedWithCustomError(crowdFunding, "CannotWithdrawFromSuccessfulProject");
+        });
+    });
+});
